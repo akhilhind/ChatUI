df5fa5d69b4b91d43a304d6ce68dd3d0880ed95d
akhil <akhil@nagarro.com>
2024-08-29 17:17:37 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+const App: React.FC = () => {
+    return (
+      <div id='page'>
+      </div>
+    );
+};
+
+export default App;
---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+export const HOST_NAME = 'http://10.36.65.15';
+
+export const ENDPOINTS = {
+    chat1: {
+        port: 5665,
+        path: '/web'
+    },
+    chat2: {
+        port: 5001,
+        path: '/gpt'
+    },
+    chat3: {
+        port: 5003,
+        path: 'chat3'
+    },
+    chat4: {
+        port: 5004,
+        path: 'chat4'
+    }
+};
+
